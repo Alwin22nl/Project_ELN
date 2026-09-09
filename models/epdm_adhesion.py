2b48a25b1178d6635199b32d14d2c716c6957ad0
Alwin Bos <a.bos@zettex.com>
2026-09-09 15:33:06 +0200

    added model file for EPDM adhesion

---
+++ models/epdm_adhesion.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+@dataclass
+class EpdmAdhesionPrep:
+    sample_id: str
+    operator_id: int
+    remark: str
+
+@dataclass
+class EpdmAdhesionTest:
+    sample_id: str
+    operator_id: int
+    remark: str
+    europees: float
+    trc: float
+    carlise: float
+    rubber: float
+    copper: float
+    wood: float
+    aluminium: float
+    aluminium_anod: float
+    lead: float
+    rvs: float
+    concrete: float
+    glass: float
+    pvc: float
+    pmma: float
+    pc: float   
