1153772ee473e2b00eb9316992b07d8cd475e9de
Alwin Bos <a.bos@zettex.com>
2026-09-16 21:56:20 +0200

    fixed small bug

--- services/samples_service.py
+++ services/samples_service.py
@@ -13,10 +13,10 @@ class SamplesService:
 
         sample = Samples(
             batch_nr=batch_nr,
-            prod_date=fromisoformat(form["prod_date"]),
+            prod_date=date.fromisoformat(form["prod_date"]),
             product_id=int(form["product_id"]),
             afterstorage_required=(form.get("after_storage_required") == "true"),
-            remark=form["remark"]
+            remark=form["remark"],
         )
 
         next_sequence = self.repository.get_next_sequence(sample.product_id)
